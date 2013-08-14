f5f4e97889bb83ed2c414d9599a60e63756cc347
Xavier Antoviaque <xavier@antoviaque.org>
2013-08-14 10:22:35 -0300

    Logout: Admin `/logout` URL, with redirect to `/` (parameterized)

--- edxserver/urls.py
+++ edxserver/urls.py
@@ -5,5 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^logout/?$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='auth_logout'),
+    url(r'^logout/(?P<next_page>.*)/$', 'django.contrib.auth.views.logout', name='auth_logout_next'),
     url(r'', include('servers.urls')),
 )
